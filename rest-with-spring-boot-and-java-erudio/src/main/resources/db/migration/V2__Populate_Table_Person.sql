411aa05550f97032d3d3aa60af45c2e5ccd82b13
Pedro Oliveira <pedro.oliveira0022@gmail.com>
2023-05-20 17:40:33 -0300

    Commit Flyway

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(2, 'São Paulo', 'Pedro', 'Male', 'Silva'),
+	(3, 'Rio de Janeiro', 'Claudia', 'Female', 'de Paula'),
+	(5, 'Rio de Janeiro', 'Dornelles', 'Male', 'de Santis'),
+	(6, 'Sergipe', 'Francisnelson', 'Male', 'Valdonias'),
+	(7, 'Maranhão', 'Francis', 'Male', 'Valdonias'),
+	(8, 'Bahia', 'Marcos', 'Male', 'Araujo');
